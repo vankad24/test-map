81b6100313a4dd9617b03a5428e8891e9c1cf5cf
Vankad24 <vankad24@mail.ru>
2023-02-26 15:27:22 +0800

    Add a memory usage plot

--- algorithms/map_task/main.py
+++ algorithms/map_task/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import time
 import math
+import sys
 
 def millis():
     return time.time_ns() // 1_000_000
@@ -9,8 +10,10 @@ data = input().split()
 # data = "0 1 1 0 2 0 3 0 4 5 5 62 6 687 7 7719".split()
 n_elements = list(map(int, data[::2]))
 cpp_time = list(map(int, data[1::2]))
+cpp_mem= list(map( lambda x: round( pow(10,x)*2*4/1024,2 ), n_elements )) #sizeof(int)*2*кол-во элементов в Кб
 
 py_time = list()
+py_mem = list()
 d = dict()
 
 for i in range(8):
@@ -20,12 +23,19 @@ for i in range(8):
     for j in range(n):
         d[j]=24
     py_time.append(millis()-start)
-    
+    py_mem.append(round( sys.getsizeof(d)/1024 ,2 ))
+
 
 plt.figure()
+plt.subplot(121)# сколько всего строк графиков, сколько всего колонок, индекс в этих строках и колонках
 plt.plot(n_elements, cpp_time, label = "C++")
 plt.plot(n_elements, py_time, label = "Python")
-plt.xlabel("2*10^i bite")
+plt.xlabel("10^i elements")
 plt.ylabel("millis")
 plt.legend()
+plt.subplot(122)
+plt.plot(n_elements, cpp_mem, label = "C++")
+plt.plot(n_elements, py_mem, label = "Python")
+plt.xlabel("10^i elements")
+plt.ylabel("Kilobytes")
 plt.show()
